91b98a22a6ca50cf0d4d91b16b8c49d25f7e36ad
disnaimov <dimitriy.kiselev0@mail.ru>
2023-12-05 10:44:20 +0400

    Пройти урок 5

---
+++ demo/src/main/java/com/example/App3.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App3 extends Application{
+
+    Stage window;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        Button button = new Button("Click me");
+        button.setOnAction(e -> AllertBox.display("Title of Window", "Allert box!"));
+       
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(button);
+        Scene scene = new Scene(layout, 300, 300);
+
+       window.setScene(scene);
+       window.show();
+    }
+    
+}
